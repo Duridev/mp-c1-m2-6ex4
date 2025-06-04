cae5af01ff7e81a64a8a4e2362b865769719bfc2
duridev <duridev@outlook.com>
2025-06-04 00:14:40 -0400

    primer intento de desarrollo de ejercicio n4

--- ejercicio2-modulo1/ejercicio4.cs
+++ ejercicio2-modulo1/ejercicio4.cs
@@ -6,7 +6,18 @@ namespace ejercicio2_modulo1
     {
         static void Main(string[] args)
         {
-            
+            int sueldo, comision, ventas, totalPagar;
+            float porcentaje;
+
+            sueldo = 15000;
+            porcentaje = 0.05;
+
+            Console.WriteLine("Ingresa las ventas realizadas por el vendedor");
+            ventas = int.Parse(Console.ReadLine());
+
+            comision = int.Parse(ventas * porcentaje);
+
+            totalPagar = int.Parse(comision + sueldo);
         }
     }
 }
