61f11bbc235e547efa3be50596c64d06e627982c
duridev <duridev@outlook.com>
2025-06-04 00:00:13 -0400

    first commit

---
+++ ejercicio2-modulo1/ejercicio4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejercicio2_modulo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ pasos.md
@@ -0,0 +1,11 @@
+
+
+## Paso1
+
+# Creación del proyecto
+
+dotnet new console -o ejercicio2-modulo1 --framework net5.0
+
+# Para hacer correr el programa es
+
+dotnet run
